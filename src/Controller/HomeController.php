988f1fdd097ca098daa5c7b3ecf65aa10363b1ed
Nauno40 <batty.arnaud@hotmail.fr>
2018-04-27 16:27:27 +0200

    Passage au débug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,13 +8,32 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController{
 
+    /**
+     * @Route("/")
+     */
     // Affiche la Home Page :
     public function homePageAction(){
         return new Response("Coucou !");
     }
 
+
+    /**
+     * @Route ("/news/{slug}")
+     */
     // Affiche la page 2 :
-    public function page2Action(){
-        return new Response("Bienvenu sur la page 2 !");
+    public function show($slug){
+
+        //dump($slug, $this);
+
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        
+        return $this->render('news/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
