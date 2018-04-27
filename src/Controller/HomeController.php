2dae51b656bdafaeb7a53730d1aad57f1ba2dbaa
Nauno40 <batty.arnaud@hotmail.fr>
2018-04-27 11:59:37 +0200

    iuytre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,28 @@ namespace App\Controller;
 
 // Les Différents Bundles don't on à Besoin :
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class HomeController{
 
     // Affiche la Home Page :
+    /**
+     * 
+     * @Route("/", "sfghdfhg")
+     * 
+     */
     public function homePageAction(){
-        return new Response('Coucou !');
+        return new Response("Coucou !");
     }
 
 
+    /**
+     * @Route("/news/test")
+     */
+    public function show(){
+        return new Response("Coucou T'es sur la 2eme page !");
+        
+    }
 
 
 }
